442c8612e5a6e1c7985cde5ffab93a24c9db0779
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-12-04 12:53:35 +0530

    Changed structure

--- Models/Matlab_Scripts/controlSnakeRobot.m
+++
@@ -1,9 +0,0 @@
-function [joint_torques] = controlSnakeRobot(joint_angles, joint_velocities)
-
-% Calculate the desired joint angles for the snake robot
-desired_joint_angles = snake_robot_trajectory(time);
-
-% Calculate the joint torques required to achieve the desired joint angles
-joint_torques = snake_robot_dynamics(joint_angles, joint_velocities, desired_joint_angles);
-
-end
