2ba6d3ba6ee82e69bd83d2434f944a67dfdb32ac
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-12-06 00:50:51 +0530

    Added face detection

--- Models/Image_Classification/object_recognition.m
+++ Models/Image_Classification/object_recognition.m
@@ -1,12 +1,34 @@
 clc;
 clear;
+close all;
 
-picture = imread("capture_2.png");
-nnet = alexnet;
-% picture = camera.snapshot;
-picture = imresize(picture,[227,227]);
-label = classify(nnet, picture);
-image(picture);
-title(upper(char(label)));
-disp(char(label));
- 
+%% face detect
+% [file,path]=uigetfile('*.*','Select an image');
+% loc=strcat(path,file);
+% pic=imread(loc);
+% pic2=rgb2gray(pic);
+% %face
+% ff=vision.CascadeObjectDetector();
+% ff.MergeThreshold = 50;
+% bbox=step(ff,pic2);
+% dd=insertObjectAnnotation(pic,'Rectangle',bbox,'Face');
+% pts=detectMinEigenFeatures(pic2,'ROI',bbox);
+% dd = insertMarker(dd,pts,'+','color','green');
+% imshow(dd);hold on
+% plot(pts.Location(:,1),pts.Location(:,2));
+% hold off
+
+%% Real-time face detection
+clc;close all;
+% clear('li');
+li=webcam();
+im=snapshot(li);
+dete=vision.CascadeObjectDetector('Mouth','MergeThreshold',100);
+pp=imshow(im);
+while true
+    im=snapshot(li);
+    im2=rgb2gray(im);
+    bb=step(dete,im2);
+    im2=insertObjectAnnotation(im,'rectangle',bb,'Face');
+    imshow(im2);
+end
